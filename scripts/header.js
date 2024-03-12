7c08ec9cbff7b0e5e9e2446b39bb0906750a45c8
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2024-03-13 00:52:25 +0300

    deneme

--- scripts/header.js
+++ scripts/header.js
@@ -10,15 +10,21 @@ window.addEventListener(("scroll"),function(){
 
 let sideBar = document.querySelector(".side-bar");
 let navBtn = document.querySelector(".side-bar-btn");
+let cardGroup = document.querySelector(".card-group");
+
 
 
 navBtn.addEventListener("click",function(){
     if(sideBar.classList.contains("side-active")){
         sideBar.classList.remove("side-active");
         navBtn.classList.remove("side-active-btn");
+        cardGroup.classList.remove("side-active-card-group");
+
     }else{
         sideBar.classList.add("side-active");
         navBtn.classList.add("side-active-btn");
+        cardGroup.classList.add("side-active-card-group");
+
 
     }
 });
